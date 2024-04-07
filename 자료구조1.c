f4a1cedc7d53829f01c983b4a0cebda973af52fe
홍성민 <landsky1234@naver.com>
2024-04-08 00:19:31 +0900

    Update 자료구조1.c

--- 자료구조1.c
+++ 자료구조1.c
@@ -5,13 +5,11 @@
 #define MAX_TERMS 101
 #define MAX_VARIABLES 2
 
-// 일단 예제에 있는 것만 제대로 해보자 !
-
-// 다항식 항
+// 다항식 각 항 표현
 struct Term {
     float coef; // 항의 계수
-    char* variable; // 항의 변수
-    int expon; // 항의 지수
+    char variable[MAX_VARIABLES]; // 항의 변수
+    int expon[MAX_VARIABLES]; // 항의 지수
 };
 
 // 다항식 표현
@@ -20,107 +18,161 @@ struct Polynomial {
     int num_terms; // 항의 개수
 };
 
-// 다항식 입력
-struct Polynomial inputPolynomial() {
-    struct Polynomial poly;
-    printf("다항식의 항의 개수를 입력하세요: ");
-    scanf_s("%d", &poly.num_terms);
-    
-    // 변수는 동적 할당
-    for (int i = 0; i < poly.num_terms; ++i) {
-        poly.terms[i].variable = malloc(2 * sizeof(char));
-        if (poly.terms[i].variable == NULL) {
-            printf("동적 할당 오류 !!!!");
-            exit(1);
-        }
-    }
-
-    for (int i = 0; i < poly.num_terms; ++i) {
-        printf("다항식의 %d번째 항의 계수를 입력하세요: ", i + 1);
-        scanf_s("%f", &poly.terms[i].coef);
-
-        printf("다항식의 %d번째 항의 변수를 입력하세요: ", i + 1);
-        scanf_s(" %c", &poly.terms[i].variable);
-
-        printf("다항식의 %d번째 항의 지수를 입력하세요: ", i + 1);
-        scanf_s("%d", &poly.terms[i].expon);
-    }
-    return poly;
-}
-
 // 다항식 정보 보기
 void printEach_polynomial(struct Polynomial poly) {
-    int i = 0;
-    for (i = 0; i < poly.num_terms - 1; ++i) {
-        if (poly.terms[i].coef > 0 && i != 0) { // 첫번째 항이 +부호이면, 안보이게끔
-            printf("+");
+    int i;
+    int term_printed = 0; // 이미 항이 출력되었는지 여부를 나타내는 플래그
+
+    for (i = 0; i < poly.num_terms; ++i) {
+        if (poly.terms[i].coef != 0) { // 항이 존재하는 경우에만 출력
+            if (poly.terms[i].coef > 0 && term_printed) { // 첫번째 항이 아니고 +부호이면 추가
+                printf(" +");
+            }
+
+            // 변수가 존재하는 경우에만 출력
+            if (poly.terms[i].variable[0] != 0) {
+                printf(" %.2f", poly.terms[i].coef);
+
+                // 변수 출력
+                for (int j = 0; j < MAX_VARIABLES; ++j) {
+                    if (poly.terms[i].variable[j] != 0) {
+                        printf("%c", poly.terms[i].variable[j]);
+
+                        // 지수가 1보다 큰 경우에만 출력
+                        if (poly.terms[i].expon[j] > 1) {
+                            printf("^%d", poly.terms[i].expon[j]);
+                        }
+                    }
+                }
+            }
+            else {
+                // 변수가 없는 경우
+                printf(" %.2f", poly.terms[i].coef);
+
+                if (poly.terms[i].variable[0] == 0) {
+                    ;
+                }
+                if (poly.terms[i].variable[1] != 0) {
+                    printf("%c^%d", poly.terms[i].variable[1], poly.terms[i].expon[1]);
+                }
+            }
+            term_printed = 1; // 항이 출력되었음을 표시
         }
-        printf("%.2f%c^%d", poly.terms[i].coef, poly.terms[i].variable, poly.terms[i].expon);
     }
-    // 상수항 출력
-    if (poly.terms[i].expon == 0) {
-        if (poly.terms[i].coef > 0) {
-            printf("+");
-        }
-        printf("%.2f", poly.terms[poly.num_terms - 1].coef);
+
+    // 마지막 항 출력
+    if (!term_printed) {
+        ; // 모든 항이 없는 경우
     }
     printf("\n");
 }
-
 // 다항식 곱셈
-// 일단 같은 변수만 조합해서 해보자
-struct Polynomial multiplyPolynomial(struct Polynomial A, struct Polynomial B) {
+struct Polynomial mat(struct Polynomial A, struct Polynomial B) {
     struct Polynomial result;
     result.num_terms = 0;
 
     for (int i = 0; i < A.num_terms; ++i) {
         for (int j = 0; j < B.num_terms; ++j) {
-            if (strcmp(A.terms[i].variable, B.terms[j].variable) == 0) {
+            // 변수가 같을 때
+            if (A.terms[i].variable[0] == B.terms[j].variable[0]) {
                 result.terms[result.num_terms].coef = A.terms[i].coef * B.terms[j].coef;
-
-                result.terms[result.num_terms].variable = malloc(2 * sizeof(char));
-                strcpy(result.terms[result.num_terms].variable, A.terms[i].variable);
-                result.terms[result.num_terms++].expon = A.terms[i].expon + B.terms[j].expon;
+                result.terms[result.num_terms].variable[0] = A.terms[i].variable[0];
+                result.terms[result.num_terms].expon[0] = A.terms[i].expon[0] + B.terms[j].expon[0];
+                result.terms[result.num_terms].variable[1] = 0; // 변수가 하나일 경우, 두 번째 변수는 0으로 설정
+                result.terms[result.num_terms].expon[1] = 0; // 변수가 하나일 경우, 두 번째 지수는 0으로 설정
+                result.num_terms++;
+            }
+            // 변수가 다를 때
+            else {
+                result.terms[result.num_terms].coef = A.terms[i].coef * B.terms[j].coef;
+                result.terms[result.num_terms].variable[0] = A.terms[i].variable[0];
+                result.terms[result.num_terms].expon[0] = A.terms[i].expon[0];
+                result.terms[result.num_terms].variable[1] = B.terms[j].variable[0];
+                result.terms[result.num_terms].expon[1] = B.terms[j].expon[0];
+                result.num_terms++;
             }
         }
+
+        
     }
     return result;
 }
+// yx -> xy로 돌리기
+struct Polynomial yx2xy(struct Polynomial A) {
+    // 일단 yx인지 알아보기
+    int exist_variable_cnt = 0;
+    for (int i = 0; i < A.num_terms; ++i) {
+        if (A.terms[i].variable[0] >= 'a' && A.terms[i].variable[0] <= 'z') {
+            exist_variable_cnt++;
+        }
+        if (A.terms[i].variable[1] >= 'a' && A.terms[i].variable[1] <= 'z') {
+            exist_variable_cnt++;
+        }
+        // 여기서 이제 variable[0] = 'x'가 들어가면 무효
+        // variable[0] = 'y'가 들어가면 바꿔줘야함
+        if (exist_variable_cnt == 2) {
+            if (A.terms[i].variable[0] == 'y') {
+                char temp = A.terms[i].variable[0];
+                A.terms[i].variable[0] = A.terms[i].variable[1];
+                A.terms[i].variable[1] = temp;
+            }
+        }
+        exist_variable_cnt = 0;
+    }
+    return A;
+}
+
 int main() {
 
-    // =====================================================
     struct Polynomial A;
-
-    // A 다항식 입력
-    printf("== A 다항식 정보 입력 ==\n");
-    A = inputPolynomial();
-
-    // A 다항식 정보 보기
-    printf("A의 다항식 정보 : ");
-    printEach_polynomial(A);
-    // =====================================================
-
-
-
-    // =====================================================
-    // 다항식 B 초기화
     struct Polynomial B;
-    printf("\n== B 다항식 정보 입력 ==\n");
-    B = inputPolynomial();
-    
-    // B다항식 정보 보기 
-    printf("B의 다항식 정보 : ");
+
+    printf(" == A == \n");
+    A.num_terms = 3;
+    A.terms[0].coef = 3;
+    A.terms[0].variable[0] = 'y';
+    A.terms[0].expon[0] = 7;
+
+    A.terms[1].coef = 4;
+    A.terms[1].variable[0] = 'x';
+    A.terms[1].expon[0] = 4;
+
+    A.terms[2].coef = -1;
+    A.terms[2].variable[0] = 0;
+    A.terms[2].expon[0] = 0;
+
+    printEach_polynomial(A);
+
+    printf("== B == \n");
+    B.num_terms = 3;
+    B.terms[0].coef = 5;
+    B.terms[0].variable[0] = 'x';
+    B.terms[0].expon[0] = 4;
+
+    B.terms[1].coef = -3;
+    B.terms[1].variable[0] = 'y';
+    B.terms[1].expon[0] = 2;
+
+    B.terms[2].coef = 7;
+    B.terms[2].variable[0] = 0;
+    B.terms[2].expon[0] = 0;
+
     printEach_polynomial(B);
-    // =====================================================
-    printf("\n");
 
-    // =====================================================
-    // 다항 A, B 곱셈
-    struct Polynomial multiply_A_B;
-    multiply_A_B = multiplyPolynomial(A, B);
-    printEach_polynomial(multiply_A_B);
+    // 곱하기
+    struct Polynomial result = mat(A, B);
+    printf("== 다항식 곱셈 결과 == \n");
+    printEach_polynomial(result);
 
-    // =====================================================
-
+    // yx -> xy
+    struct Polynomial yx2xyresult = yx2xy(result);
+    printf("== 다항식 yx -> xy로 정리 결과 == \n");
+    printEach_polynomial(yx2xyresult);
+    
+    // 다항식 내 계산
+    
+    
+    
+    
     return 0;
 }
