08f08cc6a8134322454ff36e08a73adbc12a779b
홍성민 <landsky1234@naver.com>
2024-04-07 18:05:47 +0900

    Add files via upload

---
+++ 자료구조1.c
@@ -0,0 +1,111 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+#define MAX_TERMS 101
+#define MAX_VARIABLES 2
+
+// 일단 예제에 있는 것만 제대로 해보자 !
+
+// 다항식 항
+struct Term {
+    float coef; // 항의 계수
+    char* variable; // 항의 변수
+    int expon; // 항의 지수
+};
+
+// 다항식 표현
+struct Polynomial {
+    struct Term terms[MAX_TERMS]; // 항 표현
+    int num_terms; // 항의 개수
+};
+
+// 다항식 입력
+struct Polynomial inputPolynomial() {
+    struct Polynomial poly;
+    printf("다항식의 항의 개수를 입력하세요: ");
+    scanf_s(" %d", &poly.num_terms);
+
+    // 변수는 동적 할당
+    for (int i = 0; i < poly.num_terms; ++i) {
+        poly.terms[i].variable = malloc(2 * sizeof(char));
+        if (poly.terms[i].variable == NULL) 
+            printf("동적 할당 오류 !!!!");
+            exit(1);
+        }
+    }
+
+    for (int i = 0; i < poly.num_terms; ++i) {
+        printf("다항식의 %d번째 항의 계수를 입력하세요: ", i + 1);
+        scanf_s("%f", &poly.terms[i].coef);
+
+        printf("다항식의 %d번째 항의 변수를 입력하세요: ", i + 1);
+        scanf_s(" %c", &poly.terms[i].variable);
+
+        printf("다항식의 %d번째 항의 지수를 입력하세요: ", i + 1);
+        scanf_s("%d", &poly.terms[i].expon);
+    }
+    return poly;
+}
+
+
+
+// 다항식 정보 보기
+void printEach_polynomial(struct Polynomial poly) {
+    int i = 0;
+    for (i = 0; i < poly.num_terms - 1; ++i) {
+        if (poly.terms[i].coef > 0 && i != 0) { // 첫번째 항이 +부호이면, 안보이게끔
+            printf("+");
+        }
+        printf("%.2f%c^%d", poly.terms[i].coef, poly.terms[i].variable, poly.terms[i].expon);
+    }
+    // 상수항 출력
+    if (poly.terms[i].expon == 0) {
+        if (poly.terms[i].coef > 0) {
+            printf("+");
+        }
+        printf("%.2f", poly.terms[poly.num_terms - 1].coef);
+    }
+    printf("\n");
+}
+
+int main() {
+
+    // =====================================================
+    struct Polynomial A;
+
+    // A 다항식 입력
+    printf("== A 다항식 정보 입력 ==\n");
+    A = inputPolynomial();
+
+    // A 다항식 내림차순 
+
+
+
+    // A 다항식 정보 보기
+    printf("A의 다항식 정보 : ");
+    printEach_polynomial(A);
+    // =====================================================
+
+
+
+    // =====================================================
+    // 다항식 B 초기화
+    struct Polynomial B;
+    printf("\n== B 다항식 정보 입력 ==\n");
+    B = inputPolynomial();
+    printf("B의 다항식 정보 : ");
+    printEach_polynomial(B);
+    // =====================================================
+    printf("\n");
+
+    // =====================================================
+    // 다항 A, B 곱셈
+    struct Polynomial multiply_A_B;
+    //multiply_A_B = multiplyPolynomial(A, B);
+    //printEach_polynomial(multiply_A_B);
+
+    // =====================================================
+
+    return 0;
+}
